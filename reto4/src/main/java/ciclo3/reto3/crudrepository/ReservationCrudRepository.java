58ff1ba3244a62c8aec3c827516d48f5ed62e41c
MaicolMoreno <maicol.moreno.mt@correo.usa.edu.co>
2021-10-28 22:22:29 -0500

    reto4 Update -- Delete

---
+++ reto4/src/main/java/ciclo3/reto3/crudrepository/ReservationCrudRepository.java
@@ -0,0 +1,13 @@
+package ciclo3.reto3.crudrepository;
+
+import ciclo3.reto3.model.Reservation;
+import org.springframework.data.repository.CrudRepository;
+
+
+/**
+ *
+ * @author MaicolMoreno
+ */
+public interface ReservationCrudRepository extends CrudRepository<Reservation, Integer>{
+    
+}
